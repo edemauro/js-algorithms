e1f6722a5aa76453247dc04225e4262daefb78eb
Eric Demauro <erickdemauro@gmail.com>
2016-06-18 14:38:21 -0700

    added notes to singleton.

--- singleton.js
+++ singleton.js
@@ -1,21 +1,29 @@
 const singleton = (function() {
+  // private b/c of block scoping (let)
+  // if var, would be private as well b/c of function scoping
   let instance;
+  let anotherPrivateVariable = 1;
 
+  // api
   return {
     getInstance: getInstance
   }
 
+  // function to initialize the singleton
   function init() {
     let privateVariable = 'private variable';
 
     function privateMethod() {
       console.log('private method');
+      // can access b/c it is a closure (scope chain)
+      console.log(anotherPrivateVariable);
     }
 
     function publicMethod() {
-      console.log(privateVariable);
+      privateMethod();
     }
 
+    // the singleton's api
     return {
       publicMethod: publicMethod
     }
@@ -32,5 +40,7 @@ const singleton = (function() {
 
 let one = singleton.getInstance();
 let two = singleton.getInstance();
+
+one.publicMethod();
 
 console.log(one === two);
