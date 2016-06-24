f67bf0b9c6d314edf0d923aaa5afeb3ba52a3196
Eric Demauro <erickdemauro@gmail.com>
2016-06-23 20:47:42 -0700

    added comments to topological sort.

--- topological.js
+++ topological.js
@@ -7,27 +7,36 @@ const adjList = [
   [2, 0]
 ];
 
+const adjList2 = [
+  [],
+  [0],
+  [0],
+  [1,2]
+];
+
 let topSort = (adjList) => {
   let result = [];
   let visited = [];
 
-  let visit = (node) => {
+  let dfs = (node) => {
     visited.push(node);
 
     if(adjList[node]) {
       adjList[node].forEach(function(edge) {
         if(visited.indexOf(edge) === -1) {
-          visit(edge);
+          dfs(edge);
         }
       });
     }
 
+    // use queue instead of stack. all dependent nodes will be placed in the back.
     result.unshift(node);
   };
 
+  // make sure to check every node
   for(let i = 0; i < adjList.length; i++) {
     if(visited.indexOf(i) === -1) {
-      visit(i);
+      dfs(i);
     }
   }
 
