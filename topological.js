39a5101480ccaa9f2f0919b98d71697ff528f66b
Eric Demauro <erickdemauro@gmail.com>
2016-06-25 14:28:25 -0700

    refactored topological sort to check for cycles.

--- topological.js
+++ topological.js
@@ -7,40 +7,38 @@ const adjList = [
   [2, 0]
 ];
 
-const adjList2 = [
-  [],
-  [0],
-  [0],
-  [1,2]
-];
-
-let topSort = (adjList) => {
+function topSort(graph) {
   let result = [];
   let visited = [];
+  let temp = [];
 
-  let dfs = (node) => {
-    visited.push(node);
-
-    if(adjList[node]) {
-      adjList[node].forEach(function(edge) {
-        if(visited.indexOf(edge) === -1) {
-          dfs(edge);
-        }
-      });
-    }
-
-    // use queue instead of stack. all dependent nodes will be placed in the back.
-    result.unshift(node);
-  };
-
-  // make sure to check every node
-  for(let i = 0; i < adjList.length; i++) {
-    if(visited.indexOf(i) === -1) {
-      dfs(i);
+  for(let i = 0; i < graph.length; i++) {
+    if(!visited[i] && !temp[i]) {
+      topSortHelper(i, visited, temp, graph, result);
     }
   }
 
   return result;
-};
+}
 
-console.log(topSort(adjList)); // 5,4,2,3,1,0
+function topSortHelper(node, visited, temp, graph, result) {
+  temp[node] = true;
+  let neighbors = graph[node];
+
+  for(let i = 0; i < neighbors.length; i++) {
+    let n = neighbors[i];
+
+    if(temp[n]) {
+      throw new Error('The graph is not a DAG.');
+    }
+    if(!visited[n]) {
+      topSortHelper(n, visited, temp, graph, result);
+    }
+  }
+
+  temp[node] = false;
+  visited[node] = true;
+  result.unshift(node);
+}
+
+console.log(topSort(adjList));
