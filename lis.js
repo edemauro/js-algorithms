e0f25c628185c3dfdd772d77d02a18411f486e89
Eric Demauro <erickdemauro@gmail.com>
2016-06-25 15:13:16 -0700

    added further comments to LIS.

--- lis.js
+++ lis.js
@@ -5,11 +5,14 @@ Thus, brute force is O(2^n). O(n^2) dynamic programming implementation below.
 
 function lis(array) {
   let maxLength = 1;
-  let DP = [1];
+  let DP = [1]; // array to remember the length of the LIS ending at element i.
 
   for(let i = 1; i < array.length; i++) {
     DP[i] = 1;
 
+    // check DP of all indices < i and see if DP[j] + 1 > DP[i] AND
+    // element at index j is less than element at index i since we need i to 
+    // be increasing.
     for(let j = i - 1; j>= 0; j--) {
       if(DP[j] + 1 > DP[i] && array[j] < array[i]) {
         DP[i] = DP[j] + 1;
