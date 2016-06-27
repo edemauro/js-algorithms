8e1ede86dc7fb2ac918b1156b688aa4c2e26f91d
Eric Demauro <erickdemauro@gmail.com>
2016-06-27 15:10:49 -0700

    added comments to knapsack.

--- knapsack.js
+++ knapsack.js
@@ -24,7 +24,6 @@ function recValue(items, n, S) {
   if(n === 0) { return 0; }
 
   if(cache[n][S]) {
-    console.log('using cache');
     return cache[n][S];
   }
 
@@ -39,26 +38,33 @@ function recValue(items, n, S) {
 
 // bottom up
 function knapsack(items, capacity) {
+  // initialize matrix to build up solution
   let dp = new Array(items.length + 1);
   
+  // fill first column w/ 0's
   for(let i = 0; i <= items.length; i++) {
     dp[i] = new Array(capacity + 1);
     dp[i][0] = 0;
   }
 
+  // fill first row w/ 0's
   for(let i = 0; i <= capacity; i++) {
     dp[0][i] = 0;
   }
 
   for(let n = 1; n <= items.length; n++) {
     for(let w = 1; w <= capacity; w++) {
+      // don't take item n.
       let option1 = dp[n - 1][w];
 
+      // take item n if it fits + optimal value using remaining space.
       let option2 = 0;
+      // n - 1 because items array starts w/ index 0.
       if(items[n - 1].weight <= w) {
         option2 = items[n - 1].value + dp[n - 1][w - items[n - 1].weight];
       }
       
+      // select better of two options
       dp[n][w] = Math.max(option1, option2);
     }
   }
@@ -66,4 +72,4 @@ function knapsack(items, capacity) {
   return dp;
 }
 
-console.log(knapsack(items, 6));
+console.log(knapsack(items, 7));
