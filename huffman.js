87578164a6c216106505c5af95274ae444167ec5
Eric Demauro <erickdemauro@gmail.com>
2016-06-29 22:07:25 -0700

    finished huffman codes.

--- huffman.js
+++ huffman.js
@@ -2,9 +2,8 @@
   Build tree bottom-up. At every step, select two nodes with the smallest
   frequencies.
 */
-function Node(key, value) {
-  this.key = key;
-  this.value = value;
+function Node(freq) {
+  this.freq = freq;
   this.right = null;
   this.left = null;
   this.parent = null;
@@ -41,13 +40,50 @@ const alphabet = [
 ];
 
 function huffman(alphabet) {
-  // let tree = null;
+  let tree = null;
   let queue = new PriorityQueue();
 
   alphabet.forEach(character => queue.enqueue(character.freq, character));
+  let i = 5;
+  while(!tree) {
+    let node1 = queue.dequeue();
+    let node2 = queue.dequeue();
+    let parent = new Node(node1.freq + node2.freq);
 
-  // return tree;
-  return queue;
+    node1.parent = parent;
+    node2.parent = parent;
+    parent.left = node1;
+    parent.right = node2;
+
+    queue.enqueue(parent.freq, parent);
+
+    if(parent.freq == 100) {
+      tree = parent;
+    }
+  }
+
+  return tree;
 }
 
-console.log(huffman(alphabet));
+function printCodes(root, arr, top) {
+  if(root.left) {
+    arr[top] = 0;
+    printCodes(root.left, arr, top + 1);
+  }
+
+  if(root.right) {
+    arr[top] = 1;
+    printCodes(root.right, arr, top + 1);
+  }
+
+  if(!root.left && !root.right) {
+    let code = "";
+    for(let i = 0; i < top; i++) {
+      code += arr[i];
+    }
+    console.log(root.char + ": ", code);
+  }
+}
+
+let root = huffman(alphabet);
+printCodes(root, [], 0);
