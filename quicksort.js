a98d2d19def6f0adb8df0dae5e9de89420f37fdf
Eric Demauro <erickdemauro@gmail.com>
2017-01-06 20:30:03 -0800

    changed quicksort implementation.

--- quicksort.js
+++ quicksort.js
@@ -1,43 +1,27 @@
-function quickSort(array) {
-  quickSortHelper(array, 0, array.length - 1);
-}
+function quickSort(array, left, right) {
+  let pivot = array[ Math.floor((left + right) / 2) ];
+  let i = left;
+  let j = right;
 
-function quickSortHelper(array, first, last) {
-  if(first < last) {
-    let index = partition(array, first, last);
-    quickSortHelper(array, first, index - 1);
-    quickSortHelper(array, index + 1, last);
-  }
-}
+  while( i <= j ) {
+    while( array[i] < pivot ) i++;
 
-function partition(array, first, last) {
-  // set pivot to first element
-  let pivot = array[first];
-  let left = first + 1;
-  let right = last;
+    while( array[j] > pivot ) j--;
 
-  while(left < right) {
-    // keep moving left until we find an element bigger than the pivot
-    while(array[left] < pivot) {
-      left++;
-    }
-
-    // keep moving right until we find an element smaller than the pivot
-    while(array[right] > pivot) {
-      right--;
-    }
-
-    // if right hasn't crossed left, swap elements.
-    if(left < right) {
-      swap(array, left, right);
-    }
+    if( i <= j ) {
+      swap(array, i, j);
+      i++;
+      j--;
+    } 
   }
 
-  // swap right with the pivot to move it into its correct position.
-  swap(array, first, right);
+  if( left < j ) {
+    quickSort(array, left, j);
+  }
 
-  // return the split point
-  return right;
+  if( i < right ) {
+    quickSort(array, i, right);
+  }
 }
 
 function swap(array, x, y) {
@@ -47,5 +31,9 @@ function swap(array, x, y) {
 }
 
 let arr = [54,26,93,17,77,31,44,55,20];
-quickSort(arr);
+quickSort(arr, 0, arr.length - 1);
 console.log(arr);
+
+let arr2 = [100, 50, 25];
+quickSort(arr2, 0, arr2.length - 1);
+console.log(arr2);
