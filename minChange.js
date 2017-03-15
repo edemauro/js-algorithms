9deade773f1e992436d399be187a21c83b8864d7
Eric Demauro <erickdemauro@gmail.com>
2017-03-15 15:14:41 -0700

    refactored.

--- minChange.js
+++ minChange.js
@@ -1,7 +1,7 @@
 // http://interactivepython.org/runestone/static/pythonds/Recursion/DynamicProgramming.html
 // DP = recursion + memoization. Use it when there are recursive subproblems that are not independent.
 function recMC(list, change) {
-  let minCoins = change;
+  let minCoins = Infinity;
 
   if(change === 0) {
     return 0;
@@ -20,21 +20,26 @@ function recMC(list, change) {
   return minCoins;
 }
 
-console.log(recMC([1,5,10,25], 9));
+// console.log(recMC([1,5,10,22,25], 66));
 
 function dynamicMC(arr, change) {
   let minCoins = []; // array to hold min coins to make each change value.
 
   minCoins[0] = 0; // 0 coins to make 0 change
 
-  for(let i = 1; i < change + 1; i++) { // rest of entries are infinity
+  for(let i = 1; i <= change; i++) { // rest of entries are infinity
     minCoins[i] = Infinity;
   }
   
-  for(let j = 1; j < change + 1; j++) { // for all values of change
+  for(let j = 1; j <= change; j++) { // for all values of change
     for(let i = 0; i < arr.length; i++) { // try all of the coins
-      if(1 + minCoins[j - arr[i]] < minCoins[j]) { // if 1 coin + min number of coins to make remaining change
-        minCoins[j] = 1 + minCoins[j - arr[i]]; // then update min coins
+      if(arr[i] <= j) {
+        let coinValue = arr[i];
+        let numCoinsUsed = 1 + minCoins[j - coinValue];
+
+        if(numCoinsUsed < minCoins[j]) {
+          minCoins[j] = numCoinsUsed;
+        }
       }
     }
   }
@@ -42,4 +47,4 @@ function dynamicMC(arr, change) {
   return minCoins[change];
 }
 
-console.log(dynamicMC([1,5,10,25], 6));
+console.log(dynamicMC([1,5,10,22,25], 67));
